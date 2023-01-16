e53f01a79ea4cb76faadde195656c5b11636adec
+ <+>
2023-01-16 09:24:22 +0300

    completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState} from "react";
 import Item from "./Item";
-
 function ShoppingList({ items }) {
+const [selectedCategory, filterCategory] = useState(items)
+     function getSelectedCategory(e){
+      const category = e.target.value
+      const categoryItems = items.filter(items => items.category === category)
+      filterCategory(categoryItems)
+     }
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+     
+        <select name="filter" onChange={getSelectedCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +19,24 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        
+        {selectedCategory.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
   );
 }
-
 export default ShoppingList;
+
+
+
+
+
+
+
+
+
+
+
+
